da3d39a62ec6de7531b4497670493a8bb41cab80
themitcharoni <64182946+themitcharoni@users.noreply.github.com>
2020-07-29 20:19:34 -0500

    finished studio 2

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  equipmentBeingEdited: object = null;
+  equipment: object[] =[
+    {name: "Habitat Dome"},
+    {name: "Drones"},
+    {name: "Food containers"},
+    {name: "Oxygen tanks"}
+  ];
+  add(equipmentName: string) {
+    this.equipment.push({name: equipmentName});
+  }
+
+  remove(item: object) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(item: object) {
+    this.equipmentBeingEdited = item;
+ }
+
+ save(name: string, item: object) {
+  item['name'] = name;
+  this.equipmentBeingEdited = null;
+  }
 
   constructor() { }
 
